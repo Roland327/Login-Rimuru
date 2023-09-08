877803587744069d444f8152a3948d2f6122292e
Tempest <73726760+Roland327@users.noreply.github.com>
2023-09-08 05:35:54 -0500

    first

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'login-77a42',
+    appId: '1:725554057532:web:176e6a3e357e7314c60e39',
+    storageBucket: 'login-77a42.appspot.com',
+    apiKey: 'AIzaSyCnHSidsk5GN5nshFBa_m8bY8av-rMvWgo',
+    authDomain: 'login-77a42.firebaseapp.com',
+    messagingSenderId: '725554057532',
+    measurementId: 'G-328JRWGF6W',
+  },
+};
